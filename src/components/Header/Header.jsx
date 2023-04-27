772c7fb65f3996e86089858484ff9d2cd89e962d
Anatolii Rotckin <a.rotskin@gmail.com>
2023-04-27 17:47:58 +0200

    feat: сделать header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Button, ConfigProvider, Typography } from "antd";
+const { Link } = Typography;
+
+function Header() {
+   return (
+      <header className="header">
+         <div className="header__navContainer">
+            <Link to="/">
+               <div className="header__logo"></div>
+            </Link>
+            <nav className="header__navigate">
+               <ul className="header__navigateList">
+                  <li className="header__navigateListItem">Features</li>
+                  <li className="header__navigateListItem">Pricing</li>
+                  <li className="header__navigateListItem">Resourses</li>
+               </ul>
+            </nav>
+         </div>
+         <div className="header__buttonContainer">
+            <Button
+               className="header__button header__button_type_text"
+               type="text"
+               shape="round"
+            >
+               Login
+            </Button>
+            <ConfigProvider
+               theme={{
+                  token: {
+                     colorPrimary: "hsl(180, 66%, 49%)",
+                  },
+               }}
+            >
+               <Button
+                  className="header__button header__button_type_primary"
+                  type="primary"
+                  shape="round"
+               >
+                  Sign Up
+               </Button>
+            </ConfigProvider>
+         </div>
+      </header>
+   );
+}
+
+export default Header;
